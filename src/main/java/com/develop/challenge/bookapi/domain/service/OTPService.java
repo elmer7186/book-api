635862bc3bd9c97dca81dda8f590839aa5f62da4
Elmer Urrea <elmer7186@gmail.com>
2021-01-28 23:39:31 -0500

    HU-001: Tests implementation

--- src/main/java/com/develop/challenge/bookapi/domain/service/OTPService.java
+++ src/main/java/com/develop/challenge/bookapi/domain/service/OTPService.java
@@ -1,8 +1,10 @@
 package com.develop.challenge.bookapi.domain.service;
 
+import lombok.AllArgsConstructor;
 import org.apache.commons.lang3.RandomStringUtils;
 import org.springframework.stereotype.Service;
 
+@AllArgsConstructor
 @Service
 public class OTPService {
 
