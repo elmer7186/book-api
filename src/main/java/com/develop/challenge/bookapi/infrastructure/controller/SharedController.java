b9ef0dcfad27f8701324c77a1c265645cd2ebfcb
Elmer Urrea <elmer7186@gmail.com>
2021-01-30 19:01:32 -0500

    HU-001: Add role authorization end-point

--- src/main/java/com/develop/challenge/bookapi/infrastructure/controller/SharedController.java
+++ src/main/java/com/develop/challenge/bookapi/infrastructure/controller/SharedController.java
@@ -5,6 +5,7 @@ import com.develop.challenge.bookapi.application.service.SharedAppService;
 import com.develop.challenge.bookapi.infrastructure.config.security.AuthenticatedUser;
 import com.develop.challenge.bookapi.infrastructure.controller.constants.ApiConstants;
 import lombok.AllArgsConstructor;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -18,6 +19,7 @@ public class SharedController {
 
     private SharedAppService sharedAppService;
 
+    @PreAuthorize("hasRole('ROLE_USER')")
     @PostMapping
     public void registerShared(@RequestBody SharedDto sharedDto) {
         AuthenticatedUser authenticatedUser = (AuthenticatedUser) SecurityContextHolder
