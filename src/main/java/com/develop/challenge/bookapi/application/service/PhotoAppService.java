b84e91340d0561b8f07bbd2520c9d5c7e49a6614
Elmer Urrea <elmer7186@gmail.com>
2021-01-31 20:14:31 -0500

    HU-008: add controller service and application layer implementation

--- src/main/java/com/develop/challenge/bookapi/application/service/PhotoAppService.java
+++ src/main/java/com/develop/challenge/bookapi/application/service/PhotoAppService.java
@@ -3,6 +3,7 @@ package com.develop.challenge.bookapi.application.service;
 import com.develop.challenge.bookapi.application.dto.photo.PhotoDto;
 import com.develop.challenge.bookapi.application.mapper.PhotoAppMapper;
 import com.develop.challenge.bookapi.domain.port.external.PhotoExternalPort;
+import com.develop.challenge.bookapi.domain.service.PhotoService;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Component;
 
@@ -13,10 +14,14 @@ import java.util.List;
 public class PhotoAppService {
 
     private final PhotoExternalPort photoExternalPort;
+    private final PhotoService photoService;
     private final PhotoAppMapper photoAppMapper;
 
     public List<PhotoDto> findAll() {
         return photoAppMapper.domainToDtoList(photoExternalPort.findAll());
     }
 
+    public List<PhotoDto> findByUserId(long userId) {
+        return photoAppMapper.domainToDtoList(photoService.findByUserId(userId));
+    }
 }
--- src/main/java/com/develop/challenge/bookapi/infrastructure/controller/PhotoController.java
+++ src/main/java/com/develop/challenge/bookapi/infrastructure/controller/PhotoController.java
@@ -2,9 +2,11 @@ package com.develop.challenge.bookapi.infrastructure.controller;
 
 import com.develop.challenge.bookapi.application.dto.photo.PhotoDto;
 import com.develop.challenge.bookapi.application.service.PhotoAppService;
+import com.develop.challenge.bookapi.infrastructure.config.security.AuthenticatedUser;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 import static com.develop.challenge.bookapi.infrastructure.controller.constants.ApiConstants.PHOTOS_V1;
+import static com.develop.challenge.bookapi.infrastructure.controller.constants.ApiConstants.RESOURCE_PHOTOS_USER_SESSION;
 
 @AllArgsConstructor
 @RestController
@@ -24,6 +27,14 @@ public class PhotoController {
     @GetMapping
     public ResponseEntity<List<PhotoDto>> findAll() {
         return ResponseEntity.ok(photoAppService.findAll());
+    }
+
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    @GetMapping(RESOURCE_PHOTOS_USER_SESSION)
+    public ResponseEntity<List<PhotoDto>> findByUserSession() {
+        AuthenticatedUser authenticatedUser = (AuthenticatedUser) SecurityContextHolder
+                .getContext().getAuthentication().getDetails();
+        return ResponseEntity.ok(photoAppService.findByUserId(authenticatedUser.getUserId()));
     }
 
 }
